b43dbb857a7dbb6e4f0ecc8cc201a59770d172b0
JRichUniversity <jakob.reichle@hs-augsburg.de>
2022-10-27 11:47:04 +0200

    Bug Fixes and optimization

--- src/com/company/Uebung1.java
+++ src/com/company/Uebung1.java
@@ -11,16 +11,25 @@ public class Uebung1 {
         printStar();
     }
 
-    static void printStar(){
-        for(int i = -SIZE; i <= SIZE;i++){
-            if(i == 0){
-                System.out.println(repeatChar(SPACE, WIDTH/2) + STAR);
+    /**
+     * Prints a Star of a given Size
+     */
+    static void printStar() {
+        for (int i = -SIZE; i <= SIZE; i++) {
+            if (i == 0) {
+                System.out.println(repeatChar(SPACE, WIDTH / 2) + STAR);
             } else {
                 System.out.println(repeatChar(SPACE, SIZE - Math.abs(i) + 1) + STAR + repeatChar(SPACE, WIDTH - 2 - (2 * (SIZE - Math.abs(i) + 1))) + STAR);
             }
         }
     }
 
+    /**
+     * Multiplies a given char
+     * @param str char to multiply
+     * @param var amount of times to multiply the char
+     * @return returns the multiplied char
+     */
     static String repeatChar(String str, int var){
         return String.valueOf(str).repeat(var);
     }
